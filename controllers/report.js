48215b2c62351014c5b460be0b51d380ac7f7036
entropy88 <entropystarrover@gmial.com>
2023-01-24 10:07:13 +0200

    report visualizes classif

--- controllers/report.js
+++ controllers/report.js
@@ -1,5 +1,36 @@
 module.exports={
-    report: (req,res)=>{
-        res.render('report',{title:'Отчет'})
+    report: async (req, res)=>{
+        const refs=await req.storage.getAll(req.query);
+      console.log(refs);
+    const clasif={
+       "0. Общ отдел":0,
+       "1. Философия":0,
+       "2. Религия":0,
+       "3. Обществени науки":0,
+       "5. Математика. Естествени науки":0,
+       "61. Медицина":0,
+       "62. Техника":0,
+       "63. Селско стопанство":0,
+       "64-69. Промишленост":0,
+       "7. Изкуство":0,
+       "793-799. Спорт":0,
+       "80. Езикознание":0,
+       "82. Литературознание":0,       
+       "9. История":0,
+       "91. География. Пътешествия":0
+    }
+
+    const rowClass=Object.keys(clasif);
+    console.log(rowClass)
+
+        const ctx= {
+            title: "Отчет",
+            refs,
+            clasif,
+            rowClass
+           
+        }
+
+        res.render('report',ctx);
     }
 }
