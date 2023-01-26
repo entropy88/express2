3256119b72eb1433f727af1609b918a65c08dd7d
entropy88 <entropystarrover@gmial.com>
2023-01-26 10:30:51 +0200

    edit now preselects, yay

--- controllers/edit.js
+++ controllers/edit.js
@@ -2,12 +2,44 @@ module.exports = {
     async edit(req, res) {
         const ref = await req.storage.getById(req.params.id);
 
+        //to pass type selected value
+
+        let selectedRefType = {
+            bt: false,
+            mt: false,
+            phd: false,
+            oth: false
+        }
+
+        //  "Бакалавърска дипломна работа", "Магистърска дипломна работа","Дисертация","Друго"
+
+
+        switch (ref.refType) {
+            case "Бакалавърска дипломна работа":
+                selectedRefType.bt = true;
+                break;
+            case "Магистърска дипломна работа":
+                selectedRefType.mt = true;
+                break;
+            case "Дисертация":
+                selectedRefType.phd = true;
+                break;
+            case "Друго":
+                selectedRefType.oth = true;
+                break;
+        }
+
+        console.log('selected ref type: '+ selectedRefType.bt + selectedRefType.mt+ selectedRefType.phd + selectedRefType.oth)
+
+
+
         if (!ref) {
             res.redirect('404');
         } else {
             const ctx = {
                 title: 'Редактирай справка',
-                ref
+                ref,
+                selectedRefType
             }
             res.render('edit', ctx);
         };
@@ -25,15 +57,15 @@ module.exports = {
             refStatus: req.body.refStatus
         };
         try {
-            console.log(ref);          
-           await req.storage.edit(req.params.id, ref);
+            console.log(ref);
+            await req.storage.edit(req.params.id, ref);
             res.redirect('/');
 
         } catch (err) {
-            console.log(err+ "from edit.js line 33")
+            console.log(err + "from edit.js line 33")
             res.redirect('/404');
         }
     }
 
-  
+
 };
